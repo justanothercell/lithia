41884393f70290300d10630ff26a061658589927
DragonFighter603 <75088766+DragonFIghter603@users.noreply.github.com>
2022-12-31 00:20:56 +0100

    start AST

---
+++ src/ast/mod.rs
@@ -0,0 +1,9 @@
+enum Expr {
+    Literal(AstLiteral),
+    Variable(Ident),
+    FuncCall(Item, Vec<Expression>),
+    BinaryOp(Operator, Box<Expression>, Box<Expression>),
+    UnaryOp(Operator, Box<Expression>)
+}
+
+struct Operator(pub(crate) Op, pub(crate) Span);
